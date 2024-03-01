f9b644d5951f81ae55a0633d82e59a209480bc1b
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-29 22:48:58 -0500

    working auth part 1

--- blogauth/urls.py
+++ blogauth/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import login_with_ion, AuthResult
+from .views import login_with_ion, auth_code
 
 urlpatterns = [
     path("", login_with_ion, name='login'),
-    path("auth-code", AuthResult.as_view(), name='ion-result')
+    path("auth-code", auth_code, name='ion-result')
 ]
--- blogauth/views.py
+++ blogauth/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
-from secrets import CLIENT_ID, CLIENT_SECRET
+from django.shortcuts import redirect, render
+from secrets import CLIENT_ID, CLIENT_SECRET  # type: ignore
 
 
 def login_with_ion(_):
     kwargs = {
         "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET
+        "client_secret": CLIENT_SECRET,
+        "response_type": "code"
     }
     url = "https://ion.tjhsst.edu/oauth/authorize/"
     if kwargs:
@@ -18,5 +18,13 @@ def login_with_ion(_):
     return redirect(url.rstrip("&"))
 
 
-class AuthResult(TemplateView):
-    template_name = 'auth.html'
+def auth_code(request):
+    code = request.GET.get("code", None)
+    if code is None:
+        return render(
+            request,
+            'failed.html',
+            {"description": "Did you prevent Ion access?"}
+        )
+    print(code)
+    return render(request, 'logged_in.html')
